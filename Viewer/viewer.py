f5b8644237477de7613875bc1649498cd32334a2
MxpleSticks <blf25@icloud.com>
2026-06-04 15:45:00 -0400

    Added taskbar and titlebar icon

--- Viewer/viewer.py
+++ Viewer/viewer.py
@@ -1,11 +1,14 @@
 import dearpygui.dearpygui as dpg
+from pathlib import Path
+
+ICON_PATH = Path(__file__).resolve().parents[1] / "Kobon.ico"
 
 dpg.create_context()
 
 with dpg.window(label="Viewport"):
     dpg.add_text("Test")
 
-dpg.create_viewport(title="Project Kobon", width=600, height=600)
+dpg.create_viewport(title="Project Kobon", width=600, height=600, small_icon=str(ICON_PATH), large_icon=str(ICON_PATH))
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
