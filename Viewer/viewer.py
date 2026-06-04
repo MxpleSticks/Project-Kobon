021b5218e201d935023d26801b90aabdbde8f0f6
MxpleSticks <blf25@icloud.com>
2026-06-03 23:10:26 -0400

    added basic window for viewer.py

--- Viewer/viewer.py
+++ Viewer/viewer.py
@@ -1,1 +1,14 @@
 import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+
+with dpg.window(label="Viewport"):
+    dpg.add_text("Test")
+
+dpg.create_viewport(title="Project Kobon", width=600, height=600)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+
+dpg.destroy_context()
