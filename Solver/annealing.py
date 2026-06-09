f0010a26c3b5356e72cb3c08967daee1dcf5c236
MxpleSticks <blf25@icloud.com>
2026-06-09 17:49:13 -0400

    annealing.py setup done :)

--- Solver/annealing.py
+++ Solver/annealing.py
@@ -1,5 +1,10 @@
 import numpy as np
+import random
 
 from Solver.geometry import count_triangles
 from Solver.state import create_random_state, nudge
+from Theory.theory import tamura_upper_bound
+
+startTemperature = 10.0
+coolingRate = 0.995
 
