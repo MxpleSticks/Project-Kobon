83820131dacfa107391282b5706f6c8abe5d9c21
MxpleSticks <blf25@icloud.com>
2026-06-12 17:52:59 -0400

    woopsies

--- Viewer/viewer.py
+++ Viewer/viewer.py
@@ -12,7 +12,7 @@ with dpg.window(label="Instructions (usage guide)", modal=True, show=True, tag="
         dpg.add_text(
             "Welcome to Project Kobon's output viewer! \n \n"
             
-            "Import the file your Discord Webhook outputed into the imports field (right side) to \n"
+            "Import the file your Discord Webhook outputed into the imports field (right side)\n"
             "to view it."
         )
     
